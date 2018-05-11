981065dee9d4e7aeaa24c201bc8b4b522c74c5ea
tom.lee : [Null].[None].[nil] <liyuanjunmail@qq.com>
2018-05-11 15:20:26 +0800

    Update install_libsodium.sh

--- ubuntu/install_libsodium.sh
+++ ubuntu/install_libsodium.sh
@@ -5,6 +5,7 @@ cd /tmp/libsodium-temp
 sudo add-apt-repository ppa:hzwhuang/ss-qt5
 sudo apt-get update
 sudo apt-get install shadowsocks-qt5
+sudo apt-get install build-essential
 wget https://download.libsodium.org/libsodium/releases/LATEST.tar.gz
 tar xvf LATEST.tar.gz && cd libsodium-stable/
 sudo ./configure && sudo make -j4 && sudo make install && sudo ldconfig
